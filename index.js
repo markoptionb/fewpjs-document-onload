d2e469f1155e2ad1ec80961e507936612cd37626
markoptionb <optionbsupport@gmail.com>
2021-03-16 23:18:36 +1100

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Your code goes here
+document.addEventListener("DOMContentLoaded", function () {
+    updateText()   
+});
+
+function updateText() {
+    document.getElementById('text')
+    text.innerHTML = 'This is really cool!'
+}
+
+// let textContent = 'This is really cool'
+// Element.innerHTML = textContent
+// document.querySelector( 'p' ).textContent
+
+
+ 
